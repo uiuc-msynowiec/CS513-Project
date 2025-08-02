b3018c377272602d107872e2dd57c330e5f40412
aravni2uiuc <aravni2@illinois.edu>
2025-08-02 13:42:59 -0500

    removed index from menuitems_cleaned

--- NYPL-menus/menuitems_cleaning.py
+++ NYPL-menus/menuitems_cleaning.py
@@ -12,4 +12,6 @@ data[data.price == np.nan].price = 0
 
 data = data[data.price > 0]
 
-data.to_csv('MenuItem_cleaned.csv', index=True)
+data.to_csv('MenuItem_cleaned.csv', index=False)
+
+print(data.shape)
